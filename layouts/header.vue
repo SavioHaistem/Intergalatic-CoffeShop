fec8e975c56c95db1139afcd3ec0041ee458b995
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-15 10:41:59 -0300

    sending to github

---
+++ layouts/header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="header">
+        <ul class="links">
+            <a href="link" class="link">build your coffe</a>
+            <a href="link" class="link">buy a coffe</a>
+            <a href="link" class="link">buy snacks</a>
+        </ul>
+    </header>
+    <slot />
+</template>
+<style scoped>
+header.header {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    color: white;
+    background-color: rgba(83, 135, 145, 0.075);
+    backdrop-filter: blur(8rem);
+    height: 6vh;
+    width: 100vw;
+}
+header.header > ul.links {
+    margin: 0px 0px;
+    padding: 0px 1rem;
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+}
+header.header > ul.links > a.link {
+    color: white;
+    text-decoration: none;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+    definePageMeta({
+        layout: "header"
+    })
+</script>
+<template>
+    <NuxtLayout/>
+    <main class="content">
+        <h3 class="description">
+            Seja bem vindo á cafeteria intergalática, iremos tentar o acolher da melhor forma possível
+            em nosso ambiente, aproveite a nossa tecnologia de ponta em quanto descansa das suas
+            cansativas viagens intergaláticas
+        </h3>
+    </main>
+</template>
+<style scoped>
+* {
+    margin: 0px 0px 0px 0px;
+    padding: 0px 0px 0px 0px;
+    border: 0px 0px 0px 0px;
+}
+main.content {
+    width: 80vmin;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+main.content > h3.description {
+    padding: 4rem;
+    color: rgb(245, 245, 245);
+    font-size: 18px;
+    line-height: 1.8rem;
+}
+@media (max-width: 729px) {
+    main.content > h3.description {
+        padding: 1rem;
+        font-size: 14px;
+        max-height: 10rem;
+        overflow: auto;
+    }
+}
+</style>
