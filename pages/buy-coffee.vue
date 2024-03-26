ef5adfda7db81664f45e28f93903a5f9de0f3aa4
SavioHaistem <savio.dantas.5858@gmail.com>
2024-03-26 15:21:30 -0300

    buyCoffee: coffees grid

--- pages/buy-coffee.vue
+++ pages/buy-coffee.vue
@@ -5,11 +5,19 @@
     </header>
     <main>
       <ul class="coffees-list">
-        {{ /*Lis automaticas*/ }}
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
+        <li class="coffee-item"></li>
       </ul>
-      <div class="coffees-table">
-        {{ /* svg*/ }}
-      </div>
+      <div class="coffees-table"></div>
     </main>
   </div>
 </template>
@@ -17,10 +25,45 @@
 <script setup lang="ts"></script>
 <style scoped>
   div.page {
+    height: 100vh;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  div.page > * {
+    border: 1px solid white;
+  }
+  h1.title {
+    max-height: 2rem;
   }
   main {
+    width: 95vw;
+    height: calc(100vh - 2rem);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  ul.coffees-list {
+    overflow-x: auto;
+    padding: 10px;
+    height: 80vh;
+    border: 1px solid white;
+    grid-template-columns: repeat(auto-fit, minmax(13rem, 1fr));
+    grid-gap: 1rem;
+    display: grid;
+    grid-auto-rows: 18rem;
+    grid-auto-columns: 20rem;
+    grid-auto-flow: row;
+  }
+  ul.coffees-list::-webkit-scrollbar-track-piece {
+    background-color: rgba(0, 0, 0, 0);
+  }
+  li.coffee-item {
+    list-style: none;
+    border-radius: 5px;
+    background-color: rgb(197, 197, 197);
+    border: 1px solid rgba(146, 146, 146, 0.507);
   }
 </style>
+
 // TODO: Make buy coffee grid coffees section
