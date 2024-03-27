e4de6e573a999595f721fa913296c0b07a93d2c6
SavioHaistem <savio.dantas.5858@gmail.com>
2024-03-27 10:20:55 -0300

    feat: configured icon and page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/public/css/main.css']
+  css: ['@/public/css/main.css'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/svgs/coffee-bean.svg',
+        },
+      ],
+      title: 'Intergalatic CoffeeShop',
+    },
+  },
 })
